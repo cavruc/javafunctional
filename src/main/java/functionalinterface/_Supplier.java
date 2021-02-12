3a3152e9c0125edb198cc21c79e67706cb29664b
verachirila <verachirila@gmail.com>
2021-02-12 14:17:50 +0200

    I added the _Supplier

---
+++ src/main/java/functionalinterface/_Supplier.java
@@ -0,0 +1,9 @@
+import java.util.function.Supplier;
+
+public class _Supplier{
+    public static void main(String[] args) {
+        System.out.println(name.get());
+    }
+    static Supplier<String> name = () ->
+            "I am the best, YES";
+}
