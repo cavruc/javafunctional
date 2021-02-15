66edb8b2667800998441a91b8a0aeeef51b482c6
verachirila <verachirila@gmail.com>
2021-02-15 12:52:50 +0200

    I watced all tutorial about optional'Java

--- src/main/java/optional/_exemple_Optional.java
+++ src/main/java/optional/_exemple_Optional.java
@@ -1,12 +1,24 @@
-package optional;
+package main.java.optional;
 
 import java.util.Locale;
 import java.util.Optional;
 
 public class _exemple_Optional {
     public static void main(String[] args) {
-        Person person = new Person("James", null);
-        System.out.println(person.getEmail().map(String::toLowerCase));
+        Person person = new Person("James", "JAMES@gmail.com");
+    /* String email = person
+            .getEmail()
+            .map(String::toLowerCase)
+            .orElse("email is not provided");
+
+        System.out.println(email); */
+
+    if (person.getEmail().isPresent()) {
+        String email = person.getEmail().get();
+        System.out.println(email.toLowerCase());
+    } else {
+        System.out.println("email is not provided");
+    }
     }
 }
 class Person {
